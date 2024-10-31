dbfae5acebfc48beba52cb6e6e2a271dc233bb7d
Ahmed Osama <ao198523@gmail.com>
2024-10-31 04:30:15 +0300

    user can add his name and add  user typing function

--- client/client.js
+++ client/client.js
@@ -1,5 +1,7 @@
 const socket = io("http://localhost:3000");
 
+let totalClients = document.getElementById("total-clients");
+
 let chatInput = document.getElementById("chat-input");
 let nameInput = document.getElementById("name-input");
 let messageForm = document.getElementById("message-form");
@@ -13,18 +15,60 @@ messageForm.addEventListener("submit", (event) => {
 function sendMsg() {
   if (chatInput.value === "") return;
 
-  socket.emit("chatMsg", chatInput.value);
+  socket.emit("chatMsg", {
+    msg: chatInput.value,
+    date: new Date(),
+    name: nameInput.value,
+  });
   chatInput.value = "";
 }
 
 socket.on("reply", (msg) => {
-  const item = document.createElement("li");
+  clearTyping();
+  const message = document.createElement("li");
   if (msg.senderId === socket.id) {
-    item.classList.add("message-right");
-    item.textContent = "You: " + msg.msg;
+    message.classList.add("message-right");
+    message.textContent = `${msg.name}: ` + msg.msg;
   } else {
-    item.classList.add("message-left");
-    item.textContent = "Sender: " + msg.msg;
+    message.classList.add("message-left");
+    message.textContent = `${msg.name}: ` + msg.msg;
   }
-  messageContainer.appendChild(item);
+  const timeStamp = document.createElement("span");
+  timeStamp.classList.add("timestamp");
+  timeStamp.textContent = `${moment(msg.date).format("ddd, hA")}`;
+
+  message.appendChild(timeStamp);
+  messageContainer.appendChild(message);
+  scrollToBottom();
 });
+
+socket.on("total-clients", (data) => {
+  totalClients.innerText = `Total Clients: ${data}`;
+});
+function scrollToBottom() {
+  messageContainer.scrollTo(0, messageContainer.scrollHeight);
+}
+chatInput.addEventListener("input", (_) => {
+  socket.emit("typing", {
+    name: `${nameInput.value} is typing...`,
+  });
+});
+chatInput.addEventListener("keyup", (_) => {
+  setTimeout(() => {
+    socket.emit("typing", {
+      name: "",
+    });
+  },1000);
+});
+socket.on("user-typing", (data) => {
+  clearTyping();
+  let p = document.createElement("p");
+  p.classList.add("feedback");
+  p.innerHTML = `${data.name}`;
+  messageContainer.appendChild(p);
+});
+function clearTyping() {
+  document.querySelectorAll("p").forEach((element) => {
+    element.parentNode.removeChild(element);
+  });
+}
--- server/index.js
+++ server/index.js
@@ -5,19 +5,29 @@ import { Server } from "socket.io";
 let server = app.listen(3000);
 const io = new Server(server, { cors: "*" });
 
+let socketConnected = new Set();
+
 io.on("connection", onConnected);
 
 function onConnected(socket) {
-  console.log("user connected", socket.id);
-  socket.on("chatMsg", (msg) => {
+  
+  socketConnected.add(socket.id);
+  io.emit("total-clients", socketConnected.size);
+  socket.on("chatMsg", ({ msg, date, name }) => {
     const messageData = {
       senderId: socket.id,
-      msg: msg,
+      msg,
+      date,
+      name,
     };
     io.emit("reply", messageData);
   });
-
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("user-typing", data);
+  });
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
+    socketConnected.delete(socket.id);
+    io.emit("total-clients", socketConnected.size);
   });
 }
